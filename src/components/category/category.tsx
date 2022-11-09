89c194a6402c6e4b6d52b7b16c7b67f68a057337
thanhnhung <nhung51555@donga.edu.vn>
2022-11-09 22:21:28 +0700

    code ui category

--- src/components/category/category.tsx
+++ src/components/category/category.tsx
@@ -1,1 +1,14 @@
-export default {};
+import Hot from "../../assets/images/hot.png";
+
+const Category = () => {
+  return (
+    <div className="flex-col items-center h-[198.21px] w-[134px] p-2 border-4 text-center bg-white	text-zinc-50 text-2xl	rounded-[30px]">
+      <img src={Hot} className="w-[110.55px] h-[106.21px] " alt="" />
+      <span className="text-center font-bold text-red-600 w-[44px] h-[33px]">
+        Hot
+      </span>
+    </div>
+  );
+};
+
+export default Category;
