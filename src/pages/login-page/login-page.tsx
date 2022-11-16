1a5a525aa6223669d26303841a3de021818a40b0
Bich <bich51621@donga.edu.vn>
2022-11-16 15:13:47 +0700

    login

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,50 +1,70 @@
-import React from 'react'
-import bannera from '../../assets/images/bannera.jpg'
-import {
-  BsFacebook,
-  BsApple
-} from 'react-icons/bs'
-import { FcGoogle } from 'react-icons/fc'
-import Button from '../../components/Button'
-
+import React from 'react';
+import bannera from '../../assets/images/bannera.jpg';
+import { BsFacebook, BsApple } from 'react-icons/bs';
+import { FcGoogle } from 'react-icons/fc';
+import Button from '../../components/Button';
 
 const Login = () => {
   return (
-    <div className="bg-[#FFB100] w-screen h-screen relative">
-      <div className="w-[943px] h-[553px] flex absolute left-[50%] top-[-40px] translate-x-[-50%] translate-y-[30%] ">
-        <div className="w-[100%] h-[100%] rounded-[25px]">
-          <img src={bannera} className="w-full bg-cover"></img>
-          <div className="absolute top-0 left-[43%] w-[530px] ">
-            <div className="w-[100%] h-[100] px-[70px]">
-              <p className="text-[32px] text-[#BB0707] leading-[44px] pt-[50px] text-center pb-[40px] font-bold">Đăng nhập</p>
-              <input className="w-[390px] h-[50px] border-[#BB0707] border-[3px] rounded-[20px] px-4 focus:outline-none focus-within:outline-none mb-[30px]" placeholder="Email/Số điện thoại/Tên đăng nhập"></input>
-              <input className="w-[390px] h-[50px] border-[#BB0707] border-[3px] rounded-[20px] px-4 focus:outline-none focus-within:outline-none mb-[46px]" placeholder="Mật khẩu"></input>
-              <Button
-                name="Đăng nhập"
-                className="w-[390px] h-[50px] bg-[#BB0707] rounded-[20px] text-[24px] text-center mb-[10px]"
-                onClick={() => null}
-              />
-              <p className="mb-[10px] pl-[20px]">Quên mật khẩu?</p>
-              <div className="flex justify-around">
-                <div className="w-[155px] border-[0.5px] border-[#FFB100] h-0 bg-[#FFB100]"></div>
-                <p className="mb-[20px]">Hoặc</p>
-                <div className="w-[155px] border-[0.5px] border-[#FFB100] h-0 bg-[#FFB100]"></div>
-              </div>
-              <div className="flex justify-around pb-[20px]">
-                <BsFacebook className="text-blue-600 text-[27px]"></BsFacebook>
-                <BsApple className="text-[30px]" />
-                <FcGoogle className="text-[30px]" />
-              </div>
-              <div className="flex justify-center">
-                <p>Bạn mới biết đến chúng tôi?</p>
-                <a href="https://www.youtube.com/results?search_query=react+router+dom" className="text-[#BB0707] pl-[15px]">Đăng kí</a>
-              </div>
+    <div className="bg-[#FFB100] w-screen h-screen flex justify-center">
+      <div
+        style={{ backgroundImage: `url(${bannera})` }}
+        className="bg-cover bg-no-repeat bg-center h-full w-full sm:w-[640px] flex justify-center"
+      >
+        <div className="w-[90%] sm:w-[390px] h-[100] flex flex-col justify-center items-center">
+          <p className="text-[32px] text-[#BB0707] leading-[44px] pt-[50px] text-center pb-[40px] font-bold">
+            Đăng nhập
+          </p>
+          <input
+            className="w-full h-[50px] border-[#BB0707] border-[3px] rounded-[20px] px-4 focus:outline-none focus-within:outline-none mb-[30px]"
+            placeholder="Email/Số điện thoại/Tên đăng nhập"
+          />
+          <input
+            className="w-full h-[50px] border-[#BB0707] border-[3px] rounded-[20px] px-4 focus:outline-none focus-within:outline-none mb-[46px]"
+            placeholder="Mật khẩu"
+          ></input>
+          <Button
+            name="Đăng nhập"
+            className="w-full h-[50px] bg-[#BB0707] rounded-[20px] text-[24px] text-center mb-[10px]"
+            onClick={() => null}
+          />
+          <p className="mb-[10px] w-full">
+            <span className="cursor-pointer">Quên mật khẩu?</span>
+          </p>
+          <div className="flex justify-around items-center gap-x-[5px] w-full">
+            <div className="w-full border-[0.5px] border-[#FFB100] h-0 bg-[#FFB100] relative">
+              <span className="absolute top-1/2 left-1/2 bg-white px-[8px] translate-y-[-50%] translate-x-[-50%]">
+                Hoặc
+              </span>
             </div>
+          </div>
+          <div className="flex justify-between items-center pb-[20px] gap-x-[6px] mt-[30px] w-full">
+            <div className="flex items-center">
+              <BsFacebook className="text-blue-600 text-[27px]" />
+              <span>Facebook</span>
+            </div>
+            <div className="flex items-center">
+              <BsApple className="text-[30px]" />
+              <span>App store</span>
+            </div>
+            <div className="flex items-center">
+              <FcGoogle className="text-[30px]" />
+              <span>Google</span>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <p>Bạn mới biết đến chúng tôi?</p>
+            <a
+              href="https://www.youtube.com/results?search_query=react+router+dom"
+              className="text-[#BB0707] pl-[15px]"
+            >
+              Đăng kí
+            </a>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
