26052a6fc78e8fa090b1ca1a3bde5647c3ee27ae
我的电脑\Admin <Cuong51700@donga.edu.vn>
2022-11-19 21:15:48 +0700

    order

--- src/layouts/Content/Content.tsx
+++ src/layouts/Content/Content.tsx
@@ -59,17 +59,19 @@ const Content: React.FC<IProps> = ({ setIsOpenModal }) => {
           </div>
         </div>
         <div className="w-[80%]">
-          <div className=" flex justify-between">
-            <Button
-              name="Menu Category"
-              onClick={() => null}
-              className="text-[32px]"
-            />
-            <div className="flex justify-between items-center gap-[100px]">
+          <div className=" flex justify-between w-[100%]">
+            <div className="w-[68%]">
+              <Button
+                name="Menu Category"
+                onClick={() => null}
+                className=" text-[32px] bg-[#BB0707] w-[283px] h-[63px] flex justify-center items-center"
+              />
+            </div>
+            <div className="flex justify-between items-center gap-[100px] w-[32%] ">
               <Button
                 name="Menu Order"
                 onClick={() => null}
-                className="text-[32px]"
+                className="text-[32px] bg-[#BB0707] w-[283px] h-[63px] flex justify-center items-center"
               />
               <TbEdit
                 className="text-5xl stroke-[#BB0707] cursor-pointer"
@@ -79,7 +81,7 @@ const Content: React.FC<IProps> = ({ setIsOpenModal }) => {
           </div>
           <div className="flex mt-[46px] ">
             <div className="w-[calc(100%-389px)]">
-              <div className=" max-w-full overflow-x-scroll mr-[30px] shadow-lg shadow-gray-500 rounded-[30px]">
+              <div className=" max-w-full overflow-x-scroll scrolll mr-[30px] shadow-lg shadow-gray-500 rounded-[30px]">
                 <div className="w-fit flex justify-start gap-x-[47px]">
                   {arr.map((item) => {
                     return <Category title={item.title} image={item.image} />;
@@ -102,7 +104,9 @@ const Content: React.FC<IProps> = ({ setIsOpenModal }) => {
               </div>
             </div>
             <div>
-              <Total />
+              <Total setIsOpenOrder={function (value: React.SetStateAction<boolean>): void {
+                throw new Error('Function not implemented.');
+              }} />
             </div>
           </div>
         </div>
