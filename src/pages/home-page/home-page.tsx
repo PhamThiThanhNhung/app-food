58677dc739c55058c7c2e05dc241ab9c182a8527
thanhnhung <nhungptt2002@gmail.com>
2022-11-23 15:32:39 +0700

    update logic order and filter

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -28,7 +28,7 @@ const HomePage = () => {
   >([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPage, setTotalPage] = useState<number>(0);
-  const [productsOrder, setProductsOrder] = useState<OrderProduct[]>([]);
+  let [productsOrder, setProductsOrder] = useState<OrderProduct[]>([]);
   const [count, setCount] = useState<number>(0);
   const [activeCategory, setActiveCategory] = useState<string>('');
 
